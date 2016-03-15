1547b719fbb6cef27ee8468fcbde5d86433ccebc
zhijieli <zhijieli@outlook.com>
2016-03-15 11:28:43 +0800

    ''

--- nodejs-wechat-press.js
+++ nodejs-wechat-press.js
@@ -1,8 +1,11 @@
-﻿var express = require('express');
+﻿
+var port = process.env.port || 1337;
+
+var express = require('express');
 var app = express();
 var middlewares = require('express-middlewares-js');
-app.use('/weixin', middlewares.xmlBodyParser({
-  type: 'text/xml'
+app.use('/', middlewares.xmlBodyParser({
+    type: 'text/xml'
 }));
 
 /*
@@ -15,40 +18,40 @@ app.use('/weixin', xmlBodyParser({
 }));
 
 */
-
 var Wechat = require('nodejs-wechat');
 var opt = {
-  token: token,
-  url: '/weixin'
+    token: 'CSHToolsTeam',
+    url: '/'
 };
+
 var wechat = new Wechat(opt);
 
-app.get('/weixin', wechat.verifyRequest.bind(wechat));
-app.post('/weixin', wechat.handleRequest.bind(wechat));
+app.get('/', wechat.verifyRequest.bind(wechat));
+app.post('/', wechat.handleRequest.bind(wechat));
 
 // you can also work with other restful routes
 app.use('/api', middlewares.bodyParser());
 
-wechat.on('text', function(session) {
-  session.replyTextMessage('Hello World');
+wechat.on('text', function (session) {
+    session.replyTextMessage('Hello World');
 });
-wechat.on('image', function(session) {
-  session.replyNewsMessage([{
-    Title: '新鲜事',
-    Description: '点击查看今天的新鲜事',
-    PicUrl: 'http://..',
-    Url: 'http://..'
-  }]);
+wechat.on('image', function (session) {
+    session.replyNewsMessage([{
+        Title: '新鲜事',
+        Description: '点击查看今天的新鲜事',
+        PicUrl: 'http://..',
+        Url: 'http://..'
+    }]);
 });
-wechat.on('voice', function(session) {
-  session.replyMessage({
-    Title: 'This is Music',
-    MsgType: 'music',
-    Description: 'Listen to this music and guess ths singer',
-    MusicUrl: 'http://..',
-    HQMusicUrl: 'http://..',
-    ThumbMediaId: '..'
-  });
+wechat.on('voice', function (session) {
+    session.replyMessage({
+        Title: 'This is Music',
+        MsgType: 'music',
+        Description: 'Listen to this music and guess ths singer',
+        MusicUrl: 'http://..',
+        HQMusicUrl: 'http://..',
+        ThumbMediaId: '..'
+    });
 });
 
-app.listen(80);
+app.listen(port);
--- server.js
+++ server.js
@@ -1,57 +1,29 @@
 ﻿
 var port = process.env.port || 1337;
 
+'use strict';
+
 var express = require('express');
+var Weixin = require('weixin-apis');
 var app = express();
-var middlewares = require('express-middlewares-js');
-app.use('/', middlewares.xmlBodyParser({
-    type: 'text/xml'
-}));
 
-/*
-  Alternative way
-
-var xmlBodyParser = require('express-xml-parser');
-app.use('/weixin', xmlBodyParser({
-  type: 'text/xml',
-  limit: '1mb'
-}));
-
-*/
-var Wechat = require('nodejs-wechat');
-var opt = {
-    token: 'CSHToolsTeam',
-    url: '/'
-};
-
-var wechat = new Wechat(opt);
-
-app.get('/', wechat.verifyRequest.bind(wechat));
-app.post('/', wechat.handleRequest.bind(wechat));
-
-// you can also work with other restful routes
-app.use('/api', middlewares.bodyParser());
-
-wechat.on('text', function (session) {
-    session.replyTextMessage('Hello World');
+// 配置参数
+var weixin = new Weixin({
+    app: app,
+    appid: '你的appid（可选）',
+    appsecret: '你的secret（可选）',
+    token: 'CSHToolsTeam'
 });
-wechat.on('image', function (session) {
-    session.replyNewsMessage([{
-        Title: '新鲜事',
-        Description: '点击查看今天的新鲜事',
-        PicUrl: 'http://..',
-        Url: 'http://..'
-    }]);
-});
-wechat.on('voice', function (session) {
-    session.replyMessage({
-        Title: 'This is Music',
-        MsgType: 'music',
-        Description: 'Listen to this music and guess ths singer',
-        MusicUrl: 'http://..',
-        HQMusicUrl: 'http://..',
-        ThumbMediaId: '..'
-    });
+
+weixin.on('textMsg', function (data) {
+    var msg = {
+        toUserName: data.fromUserName,
+        fromUserName: data.toUserName,
+        msgType: 'text',
+        content: data.content
+    };
+
+    console.log("===msg===", msg);
 });
 
 app.listen(port);
